a2c2e7ba5641b3e8bb54d822763e334f2cf413ff
Dr-Voltai <59165799+Dr-Voltai@users.noreply.github.com>
2019-12-23 11:15:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NjU4NTc3MjEyMDI2NTE5NTU0.XgBy1g.bDNcWvDzvoPyJVDiM7lc1NJbdC8);
