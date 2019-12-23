a08fd83c2e52dfd5c0168decab5a4fef1f227616
Dr-Voltai <59165799+Dr-Voltai@users.noreply.github.com>
2019-12-23 11:48:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
 client.on('guildMemberAdd', member => {
 client.on('message',
 
 member.guild.channels.get('JOIN/LEAVE gamer-lounge').send('Willkommen);
+}
                                                           
 });
 
