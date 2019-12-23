da9fe36b0546e4846e100a07044ad87ee594cad2
Dr-Voltai <59165799+Dr-Voltai@users.noreply.github.com>
2019-12-23 11:21:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,51 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+package de.ISAC;
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberLeaveEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
+public class JoinListener extends ListenerAdapter {
+
+	@Override
+	public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+		Member member = event.getMember();
+		TextChannel channel; 
+		
+		
+		
+		if((channel = event.getGuild().getDefaultChannel()) != null); {
+			channel.sendMessage("Ein neuer Agent ist der Gruppe beigetreten" + member.getAsMention()).queue();
+		
+		
+		
+		}
+		
+	}
+	
+	@Override
+	public void onGuildMemberLeave(GuildMemberLeaveEvent event) {
+		Member member = event.getMember();
+		TextChannel channel; 
+		
+		
+		
+		if((channel = event.getGuild().getDefaultChannel()) != null); {
+			channel.sendMessage("Ein Agent wurde abtrünnig" + member.getAsMention()).queue();
+		
+		
+		
+		}
+		
+	}
+
+
+
+
+
+}
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
